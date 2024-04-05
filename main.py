603247ff45d572c540799b5ee04031ec6924a919
lmartene <139658376+lmartene@users.noreply.github.com>
2024-04-05 18:22:48 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -69,9 +69,9 @@ st.markdown("""
 
 st.markdown("""## Simular:
 """)
-monto_prestamo = st.number_input("Monto ", min_value=100000, max_value=5000000)
-tasa_interes = st.number_input("Tasa de interés anual", min_value=0.01, max_value=0.20)
-plazo_anios = st.number_input("Plazo del préstamo (años)", min_value=1, max_value=30)
+monto_prestamo = st.number_input("Monto ", min_value=1000, max_value=5000000000)
+tasa_interes = st.number_input("Tasa de interés anual", min_value=1, max_value=10000)
+plazo_anios = st.number_input("Plazo del préstamo (años)", min_value=1, max_value=50)
 
 
 cuota_mensual = (monto_prestamo * tasa_interes / 12) / (1 - (1 + tasa_interes / 12)**(-plazo_anios * 12))
